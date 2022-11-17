07f4d1e2a7a46296b808dcfeba4331cd6aeaf93f
prsndp <prasandeepp@gmail.com>
2022-11-17 15:41:41 +0530

    application strucgure created!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
-    <div className="App">
-     <h1>  Hello, Bhai  </h1>
-    </div>
+   <BrowserRouter>  
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/hotels' element={<List/>} />
+      <Route path='/hotels/:id' element={<Hotel/>} />
+    </Routes>
+   
+   </BrowserRouter>
   );
 }
-  
+
 export default App;
